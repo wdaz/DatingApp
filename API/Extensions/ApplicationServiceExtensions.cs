3817b18c5aab383ee20ac8552a4b29c11b09ec8b
macigboy1988 <haqverdi.r@gmail.com>
2021-07-21 14:25:00 +0400

    change con string

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -52,7 +52,7 @@ namespace API.Extensions
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}?sslmode=require";
                 }
 
                 // Whether the connection string came from the local development configuration file
